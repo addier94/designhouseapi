d95c1afe3a2f87008e3c1951a433f017608a678c
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-04-10 19:33:02 -0400

    error with {user} parameter

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -19,7 +19,7 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('signed')->only('verify');
+        // $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,6 @@ Route::group(['middleware' => ['auth:api']], function () {
 // Routes for guests only
 Route::group(['middleware' => ['guest:api']], function () {
     Route::post('register', 'Auth\RegisterController@register');
-    Route::post('verification/verify', 'Auth\VerificationController@verify')->name('verification.verify');
+    Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend');
 });
