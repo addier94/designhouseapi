f5a33207da82868bbfe7021f23eac1f9671e853b
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-04-12 13:45:58 -0400

    s3/18 User Login: Issuing JWT tokens

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,39 +2,67 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function attemptLogin(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        // attempt to issue a token to the user based on the login credentials
+        $token = $this->guard()->attempt($this->credentials($request));
+
+        if( ! $token) {
+            return false;
+        }
+
+        // Get the authenticated user
+        $user = $this->guard()->user();
+
+        if($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()) {
+            return false;
+        }
+
+        //set the user's token
+        $this->guard()->setToken($token);
+        return true;
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+
+        // get the token from the authentication guard (JWT)
+        $token = (string)$this->guard()->getToken();
+
+        // extract the expiry date of the token
+        $expiration = $this->guard()->getPayload()->get('exp');
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration
+        ]);
+    }
+
+    protected function sendFailedLoginResponse()
+    {
+        $user = $this->guard()->user();
+        if ($user instanceof MustVerifyEmail && ! $user->hasVerifiedEmail()){
+            return response()->json(["errors" => [
+                "verification" => "necesitas verificar tu cuenta de correo electrónico"
+            ]]);
+        }
+
+        throw ValidationException::withMessages([
+            $this->username() => "Correo o contraseña invalido"
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,4 +11,5 @@ Route::group(['middleware' => ['guest:api']], function () {
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('verification/verify/{user}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::post('verification/resend', 'Auth\VerificationController@resend');
+    Route::post('login', 'Auth\LoginController@login');
 });
