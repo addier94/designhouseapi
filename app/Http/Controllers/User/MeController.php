5f8c127fed683be28acf71779fa4a312a61a9bf8
alfredo fernandez <alfredofernandezcarlos@gmail.com>
2020-04-12 21:34:59 -0400

    s3/22 Introducing API Resources

--- app/Http/Controllers/User/MeController.php
+++ app/Http/Controllers/User/MeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class MeController extends Controller
@@ -10,7 +11,9 @@ class MeController extends Controller
     public function getMe()
     {
         if(auth()->check()){
-            return response()->json(['user' => auth()->user()], 200);
+            $user = auth()->user();
+            return new UserResource($user);
+            // return response()->json(['user' => auth()->user()], 200);
         }
         return response()->json(null, 401);
     }
